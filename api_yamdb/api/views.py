7ccd38359ec444b45a9cd4bd00dba638c86367b4
Andrey Kruglov <altos047@gmail.com>
2022-06-21 22:45:05 +0500

    final

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -108,7 +108,7 @@ class CommentViewSet(viewsets.ModelViewSet):
     pagination_class = PageNumberPagination
 
     def get_queryset(self):
-        review = get_object_or_404(Comment, id=self.kwargs.get('review_id'))
+        review = get_object_or_404(Review, id=self.kwargs.get('review_id'))
         return review.comments.all()
 
     def perform_create(self, serializer):
